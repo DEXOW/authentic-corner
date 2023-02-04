5f12ad63ed21ffde589e8ac737d5721ef1c51f6b
DEXOW <51286591+DEXOW@users.noreply.github.com>
2023-02-04 22:24:46 +0530

    Uploaded Web App

---
+++ src/js/main.js
@@ -0,0 +1,68 @@
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
+let info;
+
+window.onload = setTimeout(async function (){
+    await fetch('src/data.json')
+        .then(response => response.json())
+        .then(response => {
+            info = response;
+        }
+    );
+    document.body.className="";
+    $('#page-content').css({'display': 'flex'})
+    setTimeout(function(){
+        $('body').css({'overflow': 'auto'})
+    }, 1000);
+},1000);
+
+window.onload = setTimeout(function(){
+    $('.brand-name').css({'height': '20vh'});
+    $('.brand-name h1').css({'font-size': '30px'});
+    $('.brand-name p').css({'font-size': '10px'});
+    $('.scroll-pointer').css({'opacity': '0'});
+    setTimeout(function(){
+        $('.scroll-pointer').css({'display': 'none'});
+    }, 500);
+
+    document.querySelector('.contact-info .contact-num').innerHTML = `${info.contact_num}`;
+    document.querySelector('.contact-info .email').innerHTML = `${info.email}`;
+    document.querySelector('.socials a.instagram').setAttribute('href', `${info.instagram}`);
+    document.querySelector('.socials a.facebook').setAttribute('href', `${info.facebook}`);
+    document.querySelector('.socials a.twitter').setAttribute('href', `${info.twitter}`);
+},1500);
+
+function goToByScroll(id){
+    $('html,body').animate({
+        scrollTop: $("#"+id).offset().top-(0.2*vh)},
+        'slow');
+}
+
+let section = document.querySelectorAll('#page-content div');
+let lists = document.querySelectorAll('.nav-bar ul li');
+function activeLink(li) {
+    if (li.classList.contains('active')) return;
+    lists.forEach((item) => item.classList.remove('active'));
+    li.classList.add('active');
+}
+lists.forEach((item) =>
+    item.addEventListener('click', function(){
+        activeLink(this);
+    }));
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            if (document.querySelector(`[href='#${id}']`) != null){
+                const target = document.querySelector(`[href='#${id}']`).parentElement;
+                activeLink(target);
+            }
+        }
+    })
+};
---
+++ src/js/product-listing.js
@@ -0,0 +1,52 @@
+window.onload = async () => {
+    let listing;
+
+    await fetch('src/listings.json')
+        .then(response => response.json())
+        .then(response => {
+            listing = response;
+        }
+    );
+
+    let featured_cols = document.querySelectorAll('#featured .col');
+    let shop_cols = document.querySelectorAll('#shop .col');
+    let i = 0;
+    let n = 0;
+    for (let c = 0; c < listing.length; c++) {
+        if (listing[c].featured == "true") {
+            if (i >= featured_cols.length) {
+                i = 0;
+            }
+            //Inserting listings to featured_cols
+            let listingDiv = document.createElement('div');
+            listingDiv.className = 'item';
+            listingDiv.innerHTML = `
+            <img src="src/img/products/${listing[c].image_name}" alt="featured-item">
+            <div class="item-info">
+                <h3>${listing[c].name}</h3>
+                <p>LKR ${listing[c].price}</p>
+                <a href="">Buy Now</a>
+            </div>`
+
+            featured_cols[i].appendChild(listingDiv);
+            i++;
+        }
+        if (n >= shop_cols.length) {
+            n = 0;
+        }
+        //Inserting listings to featured_cols
+        let shopListingDiv = document.createElement('div');
+        shopListingDiv.className = 'item';
+        shopListingDiv.innerHTML = `
+        <img src="src/img/products/${listing[c].image_name}" alt="shop-item">
+        <div class="item-info">
+            <h3>${listing[c].name}</h3>
+            <p>LKR ${listing[c].price}</p>
+            <a href="">Buy Now</a>
+        </div>`
+
+        shop_cols[n].appendChild(shopListingDiv);
+        n++;
+        
+    }
+}
